c88c3c381598b90b69d02f7543067587da2d105d
Kevin Mikus <kevinpmikus@gmail.com>
2025-06-26 17:06:18 -0400

    Fix server binding to 0.0.0.0 for Render deployment

--- server/index.js
+++ server/index.js
@@ -158,6 +158,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: err.message });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 }); 
