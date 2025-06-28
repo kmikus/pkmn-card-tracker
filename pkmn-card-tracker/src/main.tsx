20ed1a0fb6d9c1a3396d33514800176c904d4a84
Kevin Mikus <kevinpmikus@gmail.com>
2025-06-28 16:55:49 -0400

    javascript to typescript

--- pkmn-card-tracker/src/main.tsx
+++ pkmn-card-tracker/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')! /* non-null assertion */).render(
   <StrictMode>
     <BrowserRouter>
       <App />
